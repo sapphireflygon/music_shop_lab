49b7ca1ae32af9a450393a9a3b443bbc105aa382
Jennifer Lane <jennifer.lane610@gmail.com>
2022-04-19 16:57:07 +0100

    slightly modified the select(id) function in album repo file, tried (and kinda failed) to test update() function lol

--- console.py
+++ console.py
@@ -8,13 +8,13 @@ artist1 = Artist("Queen")
 album1 = Album("A Night at the Opera", "Rock", artist1)
 album2 = Album("A Day at the Races", "Rock", artist1)
 
+
 artist_repository.save(artist1)
 album_repository.save(album2)
 album_repository.save(album1)
 
-album_repository.delete_all()
-artist_repository.delete_all()
-
+# album_repository.delete_all()
+# artist_repository.delete_all()
 
 album_repository.select_all()
 artist_repository.select_all()
--- repositories/album_repository.py
+++ repositories/album_repository.py
@@ -29,7 +29,7 @@ def select(id):
     values = [id]
     result = run_sql(sql, values)[0]
 
-    if result is not None:
+    if result is not False:
         artist = artist_repository.select(result['artist_id'])
         album = Album(result['title'], result['genre'], artist, result['id'])
     return album
@@ -47,7 +47,7 @@ def delete(id):
 
 
 def update(album):
-    sql = "UPDATE albums SET (title, genre, artist_id) VALUES (%s, %s, %s) WHERE id=%s"
+    sql = "UPDATE albums SET (title, genre, artist_id) = (%s, %s, %s) WHERE id=%s"
     values = [album.title, album.genre, album.artist.id, album.id]
     run_sql(sql, values)
 
